e91d2d0417ff844e3a7adc4f6db1c468494e9126
Hans Petter Langtangen <hpl@simula.no>
2012-04-01 10:38:58 +0200

    more text

--- doc/src/sketcher/make.sh
+++ doc/src/sketcher/make.sh
@@ -2,17 +2,31 @@
 
 main=wrap_sketcher
 doconce format html $main
-exit 1
+
+cp .ptex2tex.cfg-primer .ptex2tex.cfg
+doconce format latex $main -DPRIMER_BOOK
+ptex2tex $main
+latex $main
+makeindex $main
+latex $main
+latex $main
+dvipdf $main
+mv $main.pdf ${main}_primer.pdf
+
+cp .ptex2tex.cfg-minted .ptex2tex.cfg
+doconce format pdflatex $main
+ptex2tex -DMINTED $main
+pdflatex -shell-escape $main
+makeindex $main
+pdflatex -shell-escape $main
+pdflatex -shell-escape $main
 
 doconce format sphinx $main --skip_inline_comments
 rm -rf sphinx-rootdir
 doconce sphinx_dir author="H. P. Langtangen" version=0.1 theme=pyramid $main
 python automake-sphinx.py
 
-doconce format latex $main -DPRIMER_BOOK
-ptex2tex -DMINTED $main
-latex $main
-makeindex $main
-latex $main
-latex $main
-dvipdf $main
+cp ${main}_primer.pdf ../../tutorial/pysketcher_blue.pdf
+cp ${main}.pdf ../../tutorial/pysketcher.pdf
+#cp $main.html ../../tutorial/pysketcher.html
+cp -r sphinx-rootdir/_build/html ../../tutorial/html
--- doc/src/sketcher/make_primer.sh
+++ doc/src/sketcher/make_primer.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-doconce format latex sketcher -DPRIMER_BOOK
+doconce format latex basics -DPRIMER_BOOK
+doconce format latex implemention -DPRIMER_BOOK
+doconce format latex exercises -DPRIMER_BOOK
 # fix \code{} in figure captions
-doconce replace "Hierarchy of figure elements in an instance of class \code{Vehicle0" "Hierarchy of figure elements in an instance of class \codett{Vehicle0" sketcher.p.tex
+doconce replace "Hierarchy of figure elements in an instance of class \code{Vehicle0" "Hierarchy of figure elements in an instance of class \codett{Vehicle0" implementation.p.tex
 
-ptex2tex sketcher
+# Modify syntax in exercises
+doconce subst '\\subsection\{' '\\begin{exercise}\n\\exerentry{' exercises.p.tex
+doconce subst '\\noindent\nFilename: \\code\{(.+?)\}' 'Name of program file: \\code{\g<1>}.\n\\hfill $\\diamond$\n\\end{exercise}' exercises.p.tex
+
+cp basics.p.tex pysketcher_basics.p.tex
+cp implementation.p.tex pysketcher_impl.p.tex
+cp exercises.p.tex pysketcher_ex.p.tex
