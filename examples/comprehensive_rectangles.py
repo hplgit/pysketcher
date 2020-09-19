275c31c53441351a087adb0f67125f7e9992bbcd
Richard Vodden <richard.vodden@babylonhealth.com>
2020-09-19 19:25:14 +0100

    Fixed comprehensive rectangles

--- examples/comprehensive_rectangles.py
+++ examples/comprehensive_rectangles.py
@@ -2,11 +2,10 @@ from pysketcher import MatplotlibDraw, Point, Rectangle, Composition
 
 drawing_tool = MatplotlibDraw(
     xmin=0, xmax=20, ymin=0, ymax=20, axis=False)
-drawing_tool.set_linecolor('black')
 
 code = Rectangle(Point(4, 4), 10, 15)
-code.draw_dimensions()
+code.draw_dimensions(drawing_tool)
 fig = Composition(dict(text=code))
 
-fig.draw()
+fig.draw(drawing_tool)
 drawing_tool.display()
