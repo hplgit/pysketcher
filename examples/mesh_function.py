9648c765c76994109b060491cdf2599189199d8a
Richard Vodden <richard.vodden@babylonhealth.com>
2020-11-03 18:12:44 +0000

    Fixed mesh_function.py

--- examples/mesh_function.py
+++ examples/mesh_function.py
@@ -1,27 +1,25 @@
-from pysketcher import *
+import logging
 
-"""
-u = SketchyFunc2('$u(t)$', name_pos='end')
-n = 7
-t_mesh = [i*2.25/(n-1) for i in range(n)]
+import numpy as np
 
-u = SketchyFunc2('$u(t)$', name_pos='end')
-t_mesh = [0, 2, 4, 6, 8]
-"""
+import pysketcher as ps
+from pysketcher.backend.matplotlib import MatplotlibBackend
 
-u = SketchyFunc3()
+u = ps.SketchyFunc3()
 Nt = 5
-t_mesh = linspace(0, 6, Nt + 1)
+t_mesh = np.linspace(0, 6, Nt + 1)
 
 # Add 20% space to the left and 30% to the right of the coordinate system
 t_axis_extent = t_mesh[-1] - t_mesh[0]
+logging.info(t_axis_extent)
 t_min = t_mesh[0] - 0.2 * t_axis_extent
+logging.info(t_min)
 t_max = t_mesh[-1] + 0.3 * t_axis_extent
+logging.info(t_max)
 u_max = 1.3 * max([u(t) for t in t_mesh])
+logging.info(u_max)
 u_min = -0.2 * u_max
-
-drawing_tool._set_coordinate_system(t_min, t_max, u_min, u_max, axis=False)
-drawing_tool.set_linecolor("black")
+logging.info(u_max)
 
 r = 0.005 * (t_max - t_min)  # radius of circles placed at mesh points
 # import random; random.seed(12)
@@ -31,74 +29,75 @@ u_values = []
 for i, t in enumerate(t_mesh):
     u_value = u(t) + perturbations[i]
     u_values.append(u_value)
-    u_points[i] = Composition(
-        dict(
-            circle=Circle(point(t, u_value), r).set_filled_curves("black"),
-            u_point=Text(
-                "$u^%d$" % i,
-                point(t, u_value) + (point(0, 3 * r) if i > 0 else point(-3 * r, 0)),
-            ),
-        )
+    circle = ps.Circle(ps.Point(t, u_value), r).set_fill_color(ps.Style.Color.BLACK)
+    text = ps.Text(
+        "$u^%d$" % i,
+        ps.Point(t, u_value)
+        + (ps.Point(0.0, 3 * r) if i > 0 else ps.Point(-3 * r, 0.0)),
     )
-u_discrete = Composition(u_points)
+    u_points[i] = ps.Composition({"circle": circle, "u_point": text})
+u_discrete = ps.Composition(u_points)
 
-interpolant = Composition(
+i_lines = {}
+for i in range(1, len(t_mesh)):
+    i_lines[i] = ps.Line(
+        ps.Point(t_mesh[i - 1], u_values[i - 1]), ps.Point(t_mesh[i], u_values[i])
+    ).set_line_width(1)
+interpolant = ps.Composition(i_lines)
+
+x_axis_extent: float = t_mesh[-1] + 0.2 * t_axis_extent
+logging.info(x_axis_extent)
+axes = ps.Composition(
     {
-        i: Line(
-            point(t_mesh[i - 1], u_values[i - 1]), point(t_mesh[i], u_values[i])
-        ).set_linewidth(1)
-        for i in range(1, len(t_mesh))
-    }
-)
-
-axes = Composition(
-    dict(
-        x=Axis(
-            point(0, 0),
-            t_mesh[-1] + 0.2 * t_axis_extent,
+        "x": ps.Axis(
+            ps.Point(0.0, 0.0),
+            x_axis_extent,
             "$t$",
             label_spacing=(1 / 45.0, -1 / 30.0),
         ),
-        y=Axis(point(0, 0), 0.8 * u_max, "$u$", rotation_angle=90),
-    )
+        "y": ps.Axis(ps.Point(0.0, 0.0), 0.8 * u_max, "$u$", rotation_angle=np.pi / 2),
+    }
 )
 
 h = 0.03 * u_max  # tickmarks height
-nodes = Composition(
-    {
-        i: Composition(
-            dict(
-                node=Line(point(t, h), point(t, -h)),
-                name=Text("$t_%d$" % i, point(t, -3.5 * h)),
-            )
-        )
-        for i, t in enumerate(t_mesh)
-    }
-)
-illustration = Composition(dict(u=u_discrete, mesh=nodes, axes=axes)).set_name("fdm_u")
-drawing_tool.erase()
+i_nodes = {}
+for i, t in enumerate(t_mesh):
+    i_nodes[i] = ps.Composition(
+        {
+            "node": ps.Line(ps.Point(t, h), ps.Point(t, -h)),
+            "name": ps.Text("$t_%d$" % i, ps.Point(t, -3.5 * h)),
+        }
+    )
+
+nodes = ps.Composition(i_nodes)
+
+fig = ps.Figure(t_min, t_max, u_min, u_max, backend=MatplotlibBackend)
+
 # Draw t_mesh with discrete u points
-illustration.draw()
-drawing_tool.display()
-drawing_tool.savefig(illustration.get_name())
+illustration = ps.Composition(
+    dict(
+        u=u_discrete,
+        # mesh=nodes,
+        axes=axes,
+    )
+)
+fig.erase()
+fig.add(illustration)
+fig.show()
 
 # Add exact u line (u is a Spline Shape that applies 500 intervals by default
 # for drawing the curve)
-exact = u.set_linestyle("dashed").set_linewidth(1)
-exact.draw()
-drawing_tool.display()
-drawing_tool.savefig("%s_ue" % illustration.get_name())
+exact = u.set_line_style(ps.Style.LineStyle.DASHED).set_line_width(1)
+
+fig.add(exact)
+fig.show()
 
 # Add linear interpolant
-interpolant.draw()
-drawing_tool.display()
-drawing_tool.savefig("%s_uei" % illustration.get_name())
+fig.add(interpolant)
+fig.show()
 
 # Linear interpolant without exact, smooth line
-drawing_tool.erase()
-illustration.draw()
-interpolant.draw()
-drawing_tool.display()
-drawing_tool.savefig("%s_ui" % illustration.get_name())
-
-input()
+fig.erase()
+fig.add(illustration)
+fig.add(interpolant)
+fig.show()
--- pysketcher/sketchy_func.py
+++ pysketcher/sketchy_func.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+from pysketcher.curve import Curve
 from pysketcher.point import Point
 from pysketcher.spline import Spline
-from pysketcher.text import Text
+from pysketcher.style import Style
 
 
 class SketchyFunc1(Spline):
@@ -13,18 +14,79 @@ class SketchyFunc1(Spline):
     domain = [1, 6]
 
     def __init__(self, name=None, name_pos="start", x_min=0, x_max=6, y_min=0, y_max=2):
-
         x = np.array([0, 2, 3, 4, 5, 6])
         y = np.array([1, 1.8, 1.2, 0.7, 0.8, 0.85])
 
         def scale_array(min: float, max: float, ps: np.array):
             return min - ps.min() + ps * (max - min) / (ps.max() - ps.min())
 
-        x = scale_array(x_min, x_max, x)
-        y = scale_array(y_min, y_max, y)
-
-        points = []
-        for i in range(len(x) - 1):
-            points += [Point(x[i], y[i])]
+        xs = scale_array(x_min, x_max, x)
+        ys = scale_array(y_min, y_max, y)
+        points = Point.from_coordinate_lists(xs, ys)
 
         super().__init__(points)
+
+
+class SketchyFunc2(Curve):
+    """
+    A typical function curve used to illustrate an "arbitrary" function.
+    """
+
+    domain = [0, 2.25]
+
+    def __init__(self, x_min=0, x_max=2.25, y_min=0.046679703125, y_max=1.259375):
+        a = 0
+        b = 2.25
+        resolution = 100
+        xs = np.linspace(a, b, resolution + 1)
+        ys = self(xs)
+        # Scale x and y
+        xs = x_min - xs.min() + xs * (x_max - x_min) / (xs.max() - xs.min())
+        ys = y_min - ys.min() + ys * (y_max - y_min) / (ys.max() - ys.min())
+        points = Point.from_coordinate_lists(xs, ys)
+
+        super().__init__(points)
+
+    def __call__(self, x):
+        return 0.5 + x * (2 - x) * (0.9 - x)  # on [0, 2.25]
+
+
+class SketchyFunc3(Spline):
+    """
+    A typical function curve used to illustrate an "arbitrary" function.
+    """
+
+    domain = [0, 6]
+
+    def __init__(self, x_min=0, x_max=6, y_min=0.5, y_max=3.8):
+        xs = np.array([0, 2, 3, 4, 5, 6])
+        ys = np.array([0.5, 3.5, 3.8, 2, 2.5, 3.5])
+        # Scale x and y
+        xs = x_min - xs.min() + xs * (x_max - x_min) / (xs.max() - xs.min())
+        ys = y_min - ys.min() + ys * (y_max - y_min) / (ys.max() - ys.min())
+        points = Point.from_coordinate_lists(xs, ys)
+
+        super().__init__(points)
+        self.style.line_color = Style.Color.BLACK
+
+
+# class SketchyFunc4(Spline):
+#     """
+#     A typical function curve used to illustrate an "arbitrary" function.
+#     Can be a companion function to SketchyFunc3.
+#     """
+#     domain = [1, 6]
+#
+#     def __init__(self, name=None, name_pos='start',
+#                  xmin=0, xmax=6, ymin=0.5, ymax=1.8):
+#         x = array([0, 2, 3, 4, 5, 6])
+#         y = array([1.5, 1.3, 0.7, 0.5, 0.6, 0.8])
+#         # Scale x and y
+#         x = xmin - x.min() + x * (xmax - xmin) / (x.max() - x.min())
+#         y = ymin - y.min() + y * (ymax - ymin) / (y.max() - y.min())
+#
+#         Spline.__init__(self, x, y)
+#         self.shapes['smooth'].set_linecolor('black')
+#         if name is not None:
+#             self.shapes['name'] = Text(name, self.geometric_features()[name_pos] + point(0, 0.1))
+# 1
