c3a7e1e3fa58f39f13937d22eaa2c28ef13c1e47
Richard Vodden <richard.vodden@babylonhealth.com>
2020-11-03 19:45:37 +0000

    fixed staggered_mesh_function.py

--- pysketcher/sketchy_func.py
+++ pysketcher/sketchy_func.py
@@ -70,23 +70,20 @@ class SketchyFunc3(Spline):
         self.style.line_color = Style.Color.BLACK
 
 
-# class SketchyFunc4(Spline):
-#     """
-#     A typical function curve used to illustrate an "arbitrary" function.
-#     Can be a companion function to SketchyFunc3.
-#     """
-#     domain = [1, 6]
-#
-#     def __init__(self, name=None, name_pos='start',
-#                  xmin=0, xmax=6, ymin=0.5, ymax=1.8):
-#         x = array([0, 2, 3, 4, 5, 6])
-#         y = array([1.5, 1.3, 0.7, 0.5, 0.6, 0.8])
-#         # Scale x and y
-#         x = xmin - x.min() + x * (xmax - xmin) / (x.max() - x.min())
-#         y = ymin - y.min() + y * (ymax - ymin) / (y.max() - y.min())
-#
-#         Spline.__init__(self, x, y)
-#         self.shapes['smooth'].set_linecolor('black')
-#         if name is not None:
-#             self.shapes['name'] = Text(name, self.geometric_features()[name_pos] + point(0, 0.1))
-# 1
+class SketchyFunc4(Spline):
+    """
+    A typical function curve used to illustrate an "arbitrary" function.
+    Can be a companion function to SketchyFunc3.
+    """
+
+    domain = [1, 6]
+
+    def __init__(self, name_pos="start", x_min=0, x_max=6, y_min=0.5, y_max=1.8):
+        xs = np.array([0, 2, 3, 4, 5, 6])
+        ys = np.array([1.5, 1.3, 0.7, 0.5, 0.6, 0.8])
+        # Scale x and y
+        x = x_min - xs.min() + xs * (x_max - x_min) / (xs.max() - xs.min())
+        y = y_min - ys.min() + ys * (y_max - y_min) / (ys.max() - ys.min())
+        points = Point.from_coordinate_lists(xs, ys)
+
+        super().__init__(points)
